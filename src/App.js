16ca7c558e68f772af0ac47ec82b80544f44806f
Basruhe <sb.ruhe@gmail.com>
2020-10-14 19:22:54 +0200

    adding list component

--- src/App.js
+++ src/App.js
@@ -1,13 +1,16 @@
 import React from "react";
 import logo from "./logo.svg";
 import "./App.css";
+import DogsList from "./components/DogsList";
 
 function App() {
   return (
     <div className="App">
+      <main>
+        <DogsList />
+      </main>
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>Hello Dogslist</p>
         <a
           className="App-link"
           href="https://reactjs.org"
