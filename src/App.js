81c4a9fc6d45bf41498bfea3ac0be4ff0cf8b1cb
Basruhe <sb.ruhe@gmail.com>
2020-10-15 15:18:45 +0200

    details page and routing

--- src/App.js
+++ src/App.js
@@ -1,12 +1,21 @@
 import React from "react";
 import "./App.css";
 import DogsListContainer from "./components/DogsListContainer";
+import { Route } from "react-router-dom";
+import DogsList from "./components/DogsList";
+import DogBreedImages from "./components/DogBreedImages";
 
 class App extends React.Component {
   render() {
     return (
       <div>
-        <DogsListContainer />
+        <Route exact path="/" component={DogsListContainer} />
+        {/* <Route
+          exact
+          path="/dog-breeds/{name-of-dog-breed}"
+          component="{DogBreedImages}"
+        /> */}
+        <Route path="/dog-breeds/:breed" component={DogBreedImages} />
       </div>
     );
   }
