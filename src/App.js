298aba028da32ab765d2aeb0f2eb4f6af70e5628
Basruhe <sb.ruhe@gmail.com>
2020-10-17 18:20:27 +0200

    image components

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
 import React from "react";
 import "./App.css";
 import DogsListContainer from "./components/DogsListContainer";
+import DogBreedImagesContainer from "./components/DogBreedImagesContainer";
 import { Route } from "react-router-dom";
-import DogsList from "./components/DogsList";
-import DogBreedImages from "./components/DogBreedImages";
+// import DogsList from "./components/DogsList";
 
 class App extends React.Component {
   render() {
@@ -15,7 +15,7 @@ class App extends React.Component {
           path="/dog-breeds/{name-of-dog-breed}"
           component="{DogBreedImages}"
         /> */}
-        <Route path="/dog-breeds/:breed" component={DogBreedImages} />
+        <Route path="/dog-breeds/:breed" component={DogBreedImagesContainer} />
       </div>
     );
   }
